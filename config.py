02b2c7c567919f75492631f83e393053c9bc4d06
usernamex007 <sonalixsachin@gmail.com>
2025-02-10 22:48:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-API_ID = "28795512"
-API_HASH = "c17e4eb6d994c9892b8a8b6bfea4042a"
-BOT_TOKEN = "7589052839:AAGPMVeZpb63GEG_xXzQEua1q9ewfNzTg50"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", "28795512"))
+API_HASH = os.getenv("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7589052839:AAGPMVeZpb63GEG_xXzQEua1q9ewfNzTg50")
